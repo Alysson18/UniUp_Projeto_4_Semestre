c52e0262d6f3434aa67302aeb5b849df966e9913
Alysson <alysson.paulino@hotmail.com>
2021-10-25 23:01:09 -0300

    Alteração Grid Notas + Fotter

--- src/app/Notas/notas.jsx
+++ src/app/Notas/notas.jsx
@@ -22,41 +22,42 @@ function AlterarSenha() {
         <Menu />
 
         <div className="grid">
-            <h1>Notas</h1>
+            <h1>Avaliação</h1>
             <br />
+            <div className="tabela">
+                <table className="table table-striped table-hover table-responsive">
+                    <thead>
+                        <tr className="borda">
+                            <th scope="cool">Disciplina</th>
+                            <th scope="cool" id="av1">AV1</th>
+                            <th scope="cool" id="av2">AV2</th>
+                            <th scope="cool" id="av2">AV3</th>
 
-            <table className="table table-striped table-hover">
-                <thead>
-                    <tr className="">
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            notas.notaAluno && notas.notaAluno.map((nt) =>
+                            (
+                                <tr className="col-notas">
+                                    <td id="disciplina">{nt.disciplina}</td>
+                                    <td id='av1'>{nt.av1}</td>
+                                    <td id="av2">{nt.av2}</td>
+                                    <td id="av3">{nt.av3}</td>
 
-                        <th scope="cool">Disciplina</th>
-                        <th scope="cool" id="av1">AV1</th>
-                        <th scope="cool" id="av2">AV2</th>
-                        <th scope="cool" id="av2">AV3</th>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
 
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        notas.notaAluno && notas.notaAluno.map((nt) =>
-                        (
-                            <tr className="col-notas">
-                                <td id="disciplina">{nt.disciplina}</td>
-                                <td id='av1'>{nt.av1}</td>
-                                <td id="av2">{nt.av2}</td>
-                                <td id="av3">{nt.av3}</td>
-
-                            </tr>
-                        ))
-                    }
-                </tbody>
-
-            </table>
         </div>
 
-        <footer className="footer">
+        <footer className="fotter fixed-bottom">
             <Footer />
         </footer>
+
     </div >
 
 }
