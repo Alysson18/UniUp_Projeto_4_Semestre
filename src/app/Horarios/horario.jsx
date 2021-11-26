3674bd8b101ab9b7016ed098b93344a66293ae47
Alysson <alysson.paulino@hotmail.com>
2021-11-25 22:45:16 -0300

    Backup

--- src/app/Horarios/horario.jsx
+++ src/app/Horarios/horario.jsx
@@ -41,7 +41,7 @@ function AlterarSenha() {
                         {
                             horario.horarioAluno.map((hr) =>
                             (
-                                <tr className="col-horario">
+                                <tr className="col-hours">
                                     <td id="horario">{hr.horario}</td>
                                     <td id="Segunda">{hr.disciplina_Segunda}</td>
                                     <td id="Terca">{hr.disciplina_Terca}</td>
